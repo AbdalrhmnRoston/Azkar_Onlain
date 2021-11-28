883d36f473a98394ca32487f10a9f7c95be1a110
Abdalrhman Roston <80917742+AbdalrhmnRoston@users.noreply.github.com>
2021-11-28 22:30:33 +0200

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+    let ulMenu = document.getElementById('ul-menu');
+    let togeelHeader = document.getElementById('toggel-header');
+
+    let logo = document.getElementById('logo');
+    let azkarAlspah = document.getElementById('azkar-alspa7');
+
+
+
+    let countToggel = 0;
+
+    togeelHeader.onclick = function () {
+        countToggel += 1;
+        countToggel % 2 === 0 ? ulMenu.className = 'close-ul' : ulMenu.className = 'open-ul';
+    };
+
+
+
+
+
+
+
+
+
+
+logo.onclick = function () {
+    let landing = document.getElementById('landing');
+    azkarAlspah.style.display = 'none';
+    landing.style.display = 'block';
+}
